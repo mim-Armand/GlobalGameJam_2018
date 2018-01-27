1f5a68f57cee901969ebc2bc34127a6e40b8ab5c
TomGwin <35858798+TomGwin@users.noreply.github.com>
2018-01-27 12:13:58 -0600

    TowerPlacement
    Placing tower in tiles

---
+++ Assets/Scripts/PlaceTower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower : MonoBehaviour {
+    //instantiate a copy of the object stored in towerPrefab to create a tower, and store it in tower to manipulate it during the game.
+    public GameObject towerPrefab;
+    private GameObject tower;
+
+    private bool CanPlaceTower()
+    {
+        return tower = null;
+    }
+
+    //Method to check on what to do with the tower placing 
+     private void OnMouseUp() 
+    {
+        if (CanPlaceTower()) //if tower can be placed 
+        {
+            tower = (GameObject)
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            //extra stuff like gold deduction can be added here
+        }
+    }
+}
+
