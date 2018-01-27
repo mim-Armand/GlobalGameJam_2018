59d5d231ca9ca9a9a70387f42d86b4f317cebd33
trokair <wandos1@yahoo.com>
2018-01-27 01:06:46 -0600

    Implemented Basic Entity Structure
    Creates a Entity class that can be extended with functionality for
    moveable characters and things that use health like bases and towers.
    Movement is in for Pirates and it works.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This is the base clase which is extended by any object that has health
+ */
+public class Entity : MonoBehaviour {
+	[SerializeField]
+	protected float health;
+	[SerializeField]
+	protected float movementSpeed;
+	[SerializeField]
+	protected bool canAttack;
+	[SerializeField]
+	protected bool canMove;
+	[SerializeField]
+	protected Affiliation faction;
+
+	virtual protected void Die() {
+		Debug.Log ("DEAD");
+		GameObject.Destroy (this.gameObject);
+	}
+
+	virtual protected void Attack() {
+	}
+
+	virtual protected void Move() {
+	}
+
+	void Update() {
+		if (canMove)
+			Move();
+
+		if (canAttack)
+			Attack();
+
+		if (health <= 0)
+			Die();
+	}
+}
