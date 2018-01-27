c844f077a7fe523880dc22fcb45062eface2dab2
CliqueChaser <daferrad@gmail.com>
2018-01-27 04:17:32 -0600

    attack scripts added
    added the attack and projectile classes as scripts.

---
+++ Assets/Scripts/Attacks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacks
+{
+    public void ShootStart(GameObject projectile, GameObject attacker, GameObject defender)
+    {
+        projectile = GameObject.Instantiate(projectile, attacker.transform);
+        Projectile projectilescript = projectile.GetComponent<Projectile>();
+        if (projectilescript != null)
+        {
+            projectilescript.SetMovementDirection(defender.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    private Transform projectileclass_heading;
+    private float movementspeed_factor = .1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void SetMovementDirection(Transform attackclass_heading)
+    {
+        projectileclass_heading = attackclass_heading;
+    }
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (projectileclass_heading != null && projectileclass_heading.position != this.transform.position)
+        {
+            this.transform.LookAt(projectileclass_heading);
+            this.transform.position = this.transform.position + new Vector3(0, .01f);
+        }
+     }
+}
