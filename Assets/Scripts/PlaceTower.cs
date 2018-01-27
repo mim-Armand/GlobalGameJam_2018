5aa87aaeefc8ac2260c5b3e336d65d78f8fc811e
TomGwin <35858798+TomGwin@users.noreply.github.com>
2018-01-27 12:59:04 -0600

    new towerplacement
    some changes to the file

--- Assets/Scripts/PlaceTower.cs
+++ Assets/Scripts/PlaceTower.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaceTower : MonoBehaviour {
+public class PlaceTower : MonoBehaviour
+{
     //instantiate a copy of the object stored in towerPrefab to create a tower, and store it in tower to manipulate it during the game.
     public GameObject towerPrefab;
     private GameObject tower;
 
     private bool CanPlaceTower()
     {
-        return tower = null;
+        return tower == null;
     }
 
     //Method to check on what to do with the tower placing 
-     private void OnMouseUp() 
+    void OnMouseUp()
     {
+        Debug.Log(" Error");
         if (CanPlaceTower()) //if tower can be placed 
         {
-            tower = (GameObject)
-            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            tower = GameObject.Instantiate(towerPrefab, transform.position, Quaternion.identity);
             AudioSource audioSource = gameObject.GetComponent<AudioSource>();
             audioSource.PlayOneShot(audioSource.clip);
 
