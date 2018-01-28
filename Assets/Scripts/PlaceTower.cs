1a9680e702c9628ac7e0e372a5a23869dd22ef97
TomGwin <35858798+TomGwin@users.noreply.github.com>
2018-01-28 00:40:55 -0800

    new stuff
    some new code

---
+++ Assets/Scripts/PlaceTower.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//go with BuildManager script
+public class PlaceTower : MonoBehaviour {
+
+    public Color hoverColor;
+    private GameObject tower;
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (tower != null)
+        {
+            Debug.Log("Can't build there");
+            return;
+        }
+
+        GameObject towerToBuild = BuildManager.instance.GetTowerToBuild();
+        tower = (GameObject)Instantiate(towerToBuild, transform.position, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        if(!BuildManager.CanBuild)
+        {
+            return;
+        }
+        rend.material.color = hoverColor;
+
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
+
